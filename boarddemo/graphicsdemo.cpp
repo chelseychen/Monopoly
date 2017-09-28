0defc813c60532ac1b306f9289f099d42627289f
Chelsey <chelseychen25@gmail.com>
2017-09-27 22:37:49 -0400

    Add graphicsdemo

---
+++ boarddemo/graphicsdemo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "window.h"
+
+using namespace std;
+
+int main() {
+    Xwindow w;
+    
+    
+    //w.drawString(50, 50, "Hello!", Xwindow::Blue);
+    //w.drawBigString(50, 100, "ABCD", Xwindow::Black);
+    //w.drawString(50, 150, "Hello!", Xwindow::Blue);
+    //Xwindow w2(199, 199);
+    //w2.drawBigString(50, 100, "ABCD", Xwindow::Black);
+    //w.showAvailableFonts();
+    
+    char c;
+    cin >> c;
+    
+}
+
